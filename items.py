aa981a17b2d599d2160c16f6df9f9ad1cbcc500e
Oliver Thomas <oliver.thomas404@gmail.com>
2023-10-20 21:09:41 +0100

    Added 'pyfactorio' which takes cmd line args (still) or prompts user if no item is passed as argv.
    Added support for common aliases of items e.g. greenscience, gears,
    greencircuits etc.

---
+++ items.py
@@ -0,0 +1,60 @@
+# Why the heck are you using python m8
+# Speed, have you heard of it?
+# What about type safety?
+#! /usr/bin/env python3
+# vim:fenc=utf-8
+#
+"""
+
+"""
+
+
+# checks if the passed item was short and looks up the
+# full name, which is required by the rest of the code.
+def lookupItemAliases(item):
+
+    # Do not put plurals as the key!
+    lookupItemAliasesDict = {
+        ('gear'): 'iron_gear_wheel',
+        # belts
+        ('yellowbelt'): 'transport_belt',
+        ('redbelt'): 'fast_transport_belt',
+        ('bluebelt'): 'express_transport_belt',
+        ('yellowunderground'): 'underground_belt',
+        ('redunderground'): 'fast_underground_belt',
+        ('blueunderground'): 'express_underground_belt',
+        ('yellowsplitter'): 'splitter',
+        ('redsplitter'): 'fast_splitter',
+        ('bluesplitter'): 'express_splitter',
+        #
+        ('yellowinserter'): 'inserter',
+        ('redinserter'): 'long-handed_inserter',
+        ('blueinserter'): 'fast_inserter',
+        #
+        ('greencircuit'): 'electronic_circuit',
+        ('redcircuit'): 'advanced_circuit',
+        ('bluecircuit'): 'processing_unit',
+        #
+        ('assembler'): 'assembling_machine_1',
+        ('blueassembler'): 'assembling_machine_2',
+        ('yellowassembler', 'green_assemmbler'): 'assembling_machine_3',
+        #
+        ('redscience'): 'automation_science_pack',
+        ('greenscience'): 'logistic_science_pack',
+        ('greyscience'): 'military_science_pack',
+        ('bluescience'): 'chemical_science_pack',
+        ('purplescience'): 'production_science_pack',
+        ('yellowscience'): 'utility_science_pack',
+        ('whitescience'): 'space_science_pack'
+    }
+
+    try:
+        # remove underscores and make lowercase
+        newItem = item.replace("_", "").lower()
+        # if ends in s strip it
+        if newItem[-1] == 's':
+            newItem = newItem[:-1]
+        item = lookupItemAliasesDict[newItem]
+    except KeyError:
+        item = item
+    return item
--- recipe.py
+++ recipe.py
@@ -3,6 +3,7 @@ import pandas as pd
 import networkx as nx
 from icons import get_icon
 
+
 def scale_dictionary(d, scale):
     """
     Multiply all the values in dictionary d
@@ -12,12 +13,14 @@ def scale_dictionary(d, scale):
         d[key] *= scale
     return d
 
+
 def add_dictionaries(d1, d2):
     """
     Merge two dictionaries d1 and d2 and
     add up any common keys
     """
     return dict(Counter(d1) + Counter(d2))
+
 
 class Recipe:
     """
@@ -26,38 +29,41 @@ class Recipe:
     items that are required to create it, assembling times,
     etc.
     """
+
     def __init__(self, item_dataframe):
         """
-        
-        """      
+
+        """
         self.item = item_dataframe.iloc[0]["item"]
         self.num_produced = item_dataframe.iloc[0]["num_produced"]
         self.handcraft_time = item_dataframe.iloc[0]["handcraft_time"]
 
         # dictionary mapping item name to number required
-        self.ingredients = dict(zip(item_dataframe["resource"], item_dataframe["quantity"]))
-        
+        self.ingredients = dict(
+            zip(item_dataframe["resource"], item_dataframe["quantity"]))
+
     def __repr__(self):
         """
         Get string representation
         """
-        return (f"Recipe for {self.num_produced} {self.item} in {self.handcraft_time}s, " +
-                f"using {self.ingredients}")
+        return (
+            f"Recipe for {self.num_produced} {self.item} in {self.handcraft_time}s, "
+            + f"using {self.ingredients}")
 
-    def assemblers_required(self, throughput, assembler_crafting_speed = 1):
+    def assemblers_required(self, throughput, assembler_crafting_speed=1):
         """
         Calculate the number of assemblers required to get throughput items/second
         from this recipe. assembler_crafting_speed is 1 (for human), 0.5 for
         assembly_machine_1, 0.75 for assembly_machine_2, and 1.25 for assembly_machine_3.
-        
+
         Note: modules change the crafting speed
         """
-        return throughput * self.recipe_time(assembler_crafting_speed) / self.num_produced
-
+        return throughput * self.recipe_time(
+            assembler_crafting_speed) / self.num_produced
 
     def recipe_time(self, assembler_crafting_speed):
         """
-        Calculate the time taken to run the recipe once. Depends on the 
+        Calculate the time taken to run the recipe once. Depends on the
         crafting speed, which is:
         - 1 for human,
         - 0.5 for assembly_machine_1
@@ -67,22 +73,24 @@ class Recipe:
         Note: this is not necessarily the time to craft a single item (e.g. copper_cable)
         """
         return self.handcraft_time / assembler_crafting_speed
-            
-        
+
+
 class RecipeList:
     """
     A list of all the recipes, along with methods for performing common
     operations
     """
+
     def __init__(self, factorio_resources_ods):
-        factorio_resources = pd.read_excel(factorio_resources_ods, engine="odf")
+        factorio_resources = pd.read_excel(factorio_resources_ods,
+                                           engine="odf")
         self.recipes = {}
         for item, group in factorio_resources.groupby("item"):
             self.recipes[item] = Recipe(group)
 
     def get_raw_material_counts(self, item, raw_materials):
         """
-        Get a dictionary of all the raw materials that are required to make 
+        Get a dictionary of all the raw materials that are required to make
         the given item, in terms of the raw_materials list.
         """
         item_recipe = self.get_recipe(item)
@@ -93,12 +101,16 @@ class RecipeList:
             if ingredient in raw_materials:
                 scaled_ingredient_raw_materials = {ingredient: num_required}
             else:
-                ingredient_raw_materials = self.get_raw_material_counts(ingredient, raw_materials)
-                scaled_ingredient_raw_materials = scale_dictionary(ingredient_raw_materials, num_required)
+                ingredient_raw_materials = self.get_raw_material_counts(
+                    ingredient, raw_materials)
+                scaled_ingredient_raw_materials = scale_dictionary(
+                    ingredient_raw_materials, num_required)
 
-            all_raw_materials = add_dictionaries(all_raw_materials, scaled_ingredient_raw_materials)
-        
-        return scale_dictionary(all_raw_materials, 1.0/item_recipe.num_produced)
+            all_raw_materials = add_dictionaries(
+                all_raw_materials, scaled_ingredient_raw_materials)
+
+        return scale_dictionary(all_raw_materials,
+                                1.0 / item_recipe.num_produced)
 
     def get_item_dependencies(self, item, raw_materials):
         """
@@ -113,10 +125,11 @@ class RecipeList:
             if ingredient in raw_materials:
                 item_dependencies[ingredient] = {}
             else:
-                item_dependencies[ingredient] = self.get_item_dependencies(ingredient, raw_materials)
-                
+                item_dependencies[ingredient] = self.get_item_dependencies(
+                    ingredient, raw_materials)
+
         return item_dependencies
-    
+
     def get_recipe(self, item):
         """
         Get the Recipe corresponding to item. Raises a ValueError if the item
@@ -125,12 +138,15 @@ class RecipeList:
         try:
             return self.recipes[item]
         except:
-            raise ValueError(f"Item {item} does not exist in the recipes list. Check the resources .ods file.")
+            raise ValueError(
+                f"Item {item} does not exist in the recipes list. Check the resources .ods file."
+            )
 
-    def ingredient_assemblers_per_recipe(self, item, assembler_crafting_speed, raw_materials):
+    def ingredient_assemblers_per_recipe(self, item, assembler_crafting_speed,
+                                         raw_materials):
         """
         How many assemblers are required to sustain one assembly machine
-        continuously running the recipe to make item. Returns a dictionary 
+        continuously running the recipe to make item. Returns a dictionary
         mapping ingredient names to number of assembly machines.
 
         Note: the assembler_crafting_speed does not actually matter currently, as
@@ -146,11 +162,13 @@ class RecipeList:
             if ingredient in raw_materials:
                 # Just ignore the material, from the perspective of assembly machines
                 continue
-            
-            ingredient_recipe = self.get_recipe(ingredient)       
-            assembler_counts[ingredient] = ingredient_recipe.assemblers_required(throughput,
-                                                                                 assembler_crafting_speed)
+
+            ingredient_recipe = self.get_recipe(ingredient)
+            assembler_counts[
+                ingredient] = ingredient_recipe.assemblers_required(
+                    throughput, assembler_crafting_speed)
         return assembler_counts
+
 
 class AssemblerTree:
     """
@@ -165,9 +183,11 @@ class AssemblerTree:
     - ingredients: a list of AssemblerTrees making dependencies of this item
 
     """
-    def __init__(self, item, throughput, assembler_crafting_speed, recipes, raw_materials):
+
+    def __init__(self, item, throughput, assembler_crafting_speed, recipes,
+                 raw_materials):
         self.item = item
-        
+
         # If the item is a raw material, then set the number of assemblers to zero,
         # but still save the output throughput. This corresponds to the raw material
         # source throughput requirement
@@ -177,37 +197,40 @@ class AssemblerTree:
             self.num_assemblers = 0
         else:
             item_recipe = recipes.get_recipe(item)
-            item_recipe_time = item_recipe.recipe_time(assembler_crafting_speed)
-            self.num_assemblers = item_recipe.assemblers_required(throughput, assembler_crafting_speed)
-            
+            item_recipe_time = item_recipe.recipe_time(
+                assembler_crafting_speed)
+            self.num_assemblers = item_recipe.assemblers_required(
+                throughput, assembler_crafting_speed)
+
             # Now go through each ingredient working out its throughput requirement to sustain
             # item production
             for ingredient, num_required in item_recipe.ingredients.items():
                 ingredient_output_throughput = self.num_assemblers * num_required / item_recipe_time
-                ingredient_assembler_tree = AssemblerTree(ingredient, ingredient_output_throughput,
-                                                          assembler_crafting_speed, recipes, raw_materials)
+                ingredient_assembler_tree = AssemblerTree(
+                    ingredient, ingredient_output_throughput,
+                    assembler_crafting_speed, recipes, raw_materials)
                 self.ingredients.append(ingredient_assembler_tree)
 
     def is_raw_material(self):
         return len(self.ingredients) == 0
-                
+
     def total_raw_input_throughput(self):
         """
         Add up all the raw output throughputs over the leaf nodes (with no assembling machines), which
         are interpreted as the required input throughputs of the raw materials
         """
         total_throughput = {}
-    
+
         if self.is_raw_material():
             return {self.item: self.output_throughput}
         else:
             for ingredient_assembler_tree in self.ingredients:
-                total_throughput = add_dictionaries(total_throughput,
-                                                    ingredient_assembler_tree.total_raw_input_throughput())
+                total_throughput = add_dictionaries(
+                    total_throughput,
+                    ingredient_assembler_tree.total_raw_input_throughput())
 
         return total_throughput
-                
-                
+
     def to_dict(self):
         return {
             "item": self.item,
@@ -215,25 +238,27 @@ class AssemblerTree:
             "output_throughput": self.output_throughput,
             "ingredients": [i.to_dict() for i in self.ingredients]
         }
-    
+
     def add_node(self, G):
         """
         Append this object as a node in a networkx graph. Returns
         the index of the node which was just added.
         """
         next_node_index = G.number_of_nodes()
-        G.add_node(next_node_index,
-                   item=self.item,
-                   icon=get_icon(self.item),
-                   num_assemblers=self.num_assemblers,
-                   output_throughput=self.output_throughput,)
+        G.add_node(
+            next_node_index,
+            item=self.item,
+            icon=get_icon(self.item),
+            num_assemblers=self.num_assemblers,
+            output_throughput=self.output_throughput,
+        )
         return next_node_index
-    
-    def to_graph(self, G = None):
-        """
-        Convert the object to a networkx graph. 
 
-        The function is recursive. On the way down the tree (which 
+    def to_graph(self, G=None):
+        """
+        Convert the object to a networkx graph.
+
+        The function is recursive. On the way down the tree (which
         is a depth-first order), each ingredient (i.e. assembler node)
         is assigned a number. On the way up, edges are assigned between
         the node numbers. You cannot do this on the way down, because
@@ -243,7 +268,7 @@ class AssemblerTree:
 
         if G is None:
             G = nx.Graph()
-        
+
         current_node_index = self.add_node(G)
 
         # Now iterate over the children adding them
@@ -261,13 +286,15 @@ class AssemblerTree:
         else:
             return G, current_node_index
 
+
 class CombinedAssemblerGraph:
     """
     This is the tree of assemblers required to generate a particular throughput of
     item production at the top level, but where resources only appear once in the tree.
     Compared to AssemblerTree, this graph can contain cycles between the same resource
-    may be used to make two different items. 
+    may be used to make two different items.
     """
+
     def __init__(self, assembler_tree):
 
         # A map from strings to a dictionary of node information
@@ -282,7 +309,7 @@ class CombinedAssemblerGraph:
         """
         Add an assembler node (assembler_tree) to the nodes list. If there
         is currently no node for this item, create one, give it the next
-        free node index, and save the assembler_tree data in it. If there 
+        free node index, and save the assembler_tree data in it. If there
         is already a node, add in the num_assemblers and output_throughput.
         """
         item = assembler_tree.item
@@ -293,24 +320,26 @@ class CombinedAssemblerGraph:
             }
         else:
             self.nodes[item]["num_assemblers"] += assembler_tree.num_assemblers
-            self.nodes[item]["output_throughput"] += assembler_tree.output_throughput
-        
+            self.nodes[item][
+                "output_throughput"] += assembler_tree.output_throughput
+
     def add_assembler_tree(self, assembler_tree):
         """
-        Adds the nodes from an assembler tree to self.nodes, and 
+        Adds the nodes from an assembler tree to self.nodes, and
         adds edges to self.edges
         """
 
         # Save the assembler_tree as a node
         self.push_assembler_node(assembler_tree)
-        
+
         for ingredient_assembler_tree in assembler_tree.ingredients:
 
             # Add edges from all ingredients to the parent node
-            self.edges.add((assembler_tree.item, ingredient_assembler_tree.item))
-            
+            self.edges.add(
+                (assembler_tree.item, ingredient_assembler_tree.item))
+
             self.add_assembler_tree(ingredient_assembler_tree)
-        
+
     def __repr__(self):
         """
         Print the nodes and edges.
